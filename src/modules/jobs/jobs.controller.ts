31bbd5e6d6976fc9455c8427edf6be496049e751
DuyKhanh16 <duykhanh16121993@gmail.com>
2024-04-16 05:02:39 +0700

    add update job

--- src/modules/jobs/jobs.controller.ts
+++ src/modules/jobs/jobs.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+} from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
@@ -7,15 +16,37 @@ require('dotenv').config();
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
-  @Post("create/:id")
- async createNewJob(@Body() createJobDto: CreateJobDto,@Res() res, @Param("id") id) {
-   try {
-    await this.jobsService.createNewJob(createJobDto,id);
-    res.status(process.env.STATUS_CREATR_OK).json({ message: process.env.SUCCESS });
-   } catch (error) {
-    console.log(error);
-    res.status(process.env.STATUS_FAIL).json({ message: error.message });
-   }
-   
- }
+  @Post('create/:id')
+  async createNewJob(
+    @Body() createJobDto: CreateJobDto,
+    @Res() res,
+    @Param('id') id,
+  ) {
+    try {
+      await this.jobsService.createNewJob(createJobDto, id);
+      res
+        .status(process.env.STATUS_CREATR_OK)
+        .json({ message: process.env.SUCCESS });
+    } catch (error) {
+      console.log(error);
+      res.status(process.env.STATUS_FAIL).json({ message: error.message });
+    }
+  }
+
+  @Patch("edit/:id")
+  async editJob(
+    @Body() updateJob: UpdateJobDto,
+    @Res() res,
+    @Param('id') id,
+  ){
+    try {
+      await this.jobsService.updateJob(updateJob,id)
+      res
+      .status(process.env.STATUS_CREATR_OK)
+      .json({ message: process.env.SUCCESS });
+    } catch (error) {
+      console.log(error);
+      res.status(process.env.STATUS_FAIL).json({ message: error.message });
+    }
+  }
 }
