2cb75d2935fa673acd890dbab07364d078b456f1
hoang <khuongdanhhoang123@gmail.com>
2024-04-16 19:42:09 +0700

    updat11

--- src/modules/project_candidate/dto/create-project_candidate.dto.ts
+++ src/modules/project_candidate/dto/create-project_candidate.dto.ts
@@ -1,28 +1,10 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateProjectCandidateDto {
-
-    @IsNotEmpty()
-    @IsString()
     candidate_id:string|any;
-
-    @IsNotEmpty()
-    @IsString()
     name: string;
-
-    @IsNotEmpty()
-    @IsString()
     link: string;
-
-    @IsNotEmpty()
-    @IsString()
     info: string;
-
-    @IsNotEmpty()
-    @IsString()
     start_at: string;
-
-    @IsNotEmpty()
-    @IsString()
     end_at: string;
 }
