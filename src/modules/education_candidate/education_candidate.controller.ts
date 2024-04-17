3f0041a3369d0d110f13af7a4dee04fb83b0cd19
Kingsman <hoangbaohs22@gmail.com>
2024-04-17 23:23:34 +0700

    test

--- src/modules/education_candidate/education_candidate.controller.ts
+++ src/modules/education_candidate/education_candidate.controller.ts
@@ -1,65 +1,99 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { EducationCandidateService } from './education_candidate.service';
 import { CreateEducationCandidateDto } from './dto/create-education_candidate.dto';
 import { UpdateEducationCandidateDto } from './dto/update-education_candidate.dto';
 import { AuthGuard } from '../guard/auth.guard';
 
-@Controller('api/v2/candidate')  
+@Controller('api/v2/candidate')
 export class EducationCandidateController {
-  constructor(private readonly educationCandidateService: EducationCandidateService) {}
+  constructor(
+    private readonly educationCandidateService: EducationCandidateService,
+  ) {}
 
-
-  @Get("getAllEducationCandidate")
+  @Get('getAllEducationCandidate')
   @UseGuards(AuthGuard)
-  findEducations(@Res() res,@Req() req) {
-    console.log(req.account.email)
+  async findEducations(@Res() res, @Req() req) {
+    console.log(req.account.email);
     try {
-      const result = this.educationCandidateService.findEducations(req.account.email);
-      res.status(200).json({ 
-        message:"success",
-        data:result
-       });
+      const result = await this.educationCandidateService.findEducations(
+        req.account.email,
+      );
+      res.status(200).json({
+        message: 'success',
+        data: result,
+      });
     } catch (error) {
-      res.status(400).json({message:error})
+      res.status(400).json({ message: error });
     }
   }
 
   // thêm thông tin học vấn
-  @Post("createEducation")
-  create(@Body() body:CreateEducationCandidateDto ,@Res() res ) {
-    
-    console.log(body)
+  @Post('createEducation')
+  create(@Body() body: CreateEducationCandidateDto, @Res() res) {
+    console.log(body);
     try {
-      const {candidate_id,name_education,major,start_at,end_at,info} = body 
+      const { candidate_id, name_education, major, start_at, end_at, info } =
+        body;
       // console.log(body)
-      const result = this.educationCandidateService.createNewEducation(candidate_id,name_education,major,start_at,end_at,info)
-      res.status(201).json({message:"create success"})
+      const result = this.educationCandidateService.createNewEducation(
+        candidate_id,
+        name_education,
+        major,
+        start_at,
+        end_at,
+        info,
+      );
+      res.status(201).json({ message: 'create success' });
     } catch (error) {
-      res.status(400).json({message:error})
+      res.status(400).json({ message: error });
     }
   }
 
   //update thông tin học vấn candidate
-  @Patch("updateEducation/:id")
-  update(@Body() body:CreateEducationCandidateDto ,@Res() res ,@Param('id') id:string) {
+  @Patch('updateEducation/:id')
+  update(
+    @Body() body: CreateEducationCandidateDto,
+    @Res() res,
+    @Param('id') id: string,
+  ) {
     try {
-      const {candidate_id,name_education,major,start_at,end_at,info} = body 
+      const { candidate_id, name_education, major, start_at, end_at, info } =
+        body;
       // console.log(body)
-      const result = this.educationCandidateService.updateNewEducation(id,candidate_id,name_education,major,start_at,end_at,info)
-      res.status(200).json({message:"update education success"})
+      const result = this.educationCandidateService.updateNewEducation(
+        id,
+        candidate_id,
+        name_education,
+        major,
+        start_at,
+        end_at,
+        info,
+      );
+      res.status(200).json({ message: 'update education success' });
     } catch (error) {
-      res.status(400).json({message:error})
+      res.status(400).json({ message: error });
     }
   }
 
   @Delete('deleteEducation/:id')
-  delete(@Param('id') id: string, @Res() res) {
+  async delete(@Param('id') id: string, @Res() res) {
     try {
-      console.log(id)
-      const result = this.educationCandidateService.removeEdication(id);
-      res.status(200).json({message:"delete education success"})
+      console.log(id);
+      const result = await this.educationCandidateService.removeEdication(id);
+      res.status(200).json({ message: 'delete education success' });
     } catch (error) {
-      res.status(400).json({message:error})
+      res.status(400).json({ message: error });
     }
   }
 }
