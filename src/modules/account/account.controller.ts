a11c0c344f73169d6c9101d09e1cee291b560930
DuyKhanh16 <duykhanh16121993@gmail.com>
2024-04-24 18:42:20 +0700

    add candidate save job

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Req, UseGuards } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { AuthGuard } from '../guard/auth.guard';
 
 @Controller('api/v2/account')
 export class AccountController {
@@ -13,5 +14,17 @@ export class AccountController {
     return this.accountService.BanorUnbandAccount(+id, updateAccountDto);
   }
 
-  
+  @Get("id-auth")
+  @UseGuards(AuthGuard)
+  async getCandidateByAuth(@Res() res,@Req() req) {
+    try {
+      const result = await this.accountService.getcandidateByEmail(req.account.email);
+      res.status(200).json({
+        message:"success",
+        data:result.id
+      })
+    } catch (error) {
+      res.status(400).json({message:error})
+    }
+  }
 }
